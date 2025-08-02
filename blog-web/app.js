1d7ef6bf381bbdeda314dd60a1c7262bf261b165
Nihalxyztr <tripathinihal2006@gmail.com>
2025-08-02 14:29:13 +0530

    blog website

---
+++ blog-web/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://Nihal:NihalTripathi123@cluster0.lxvknrj.mongodb.net/blogDB?retryWrites=true&w=majority&appName=Cluster0", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log(" MongoDB connected"))
+  .catch(err => console.log(" MongoDB error:", err));
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", async (req, res) => {
+  const posts = await Blog.find().sort({ _id: -1 });
+  res.render("index", { posts });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", async (req, res) => {
+  const newPost = new Blog({
+    title: req.body.title,
+    content: req.body.content
+  });
+  await newPost.save();
+  res.redirect("/");
+});
+
+app.get("/post/:id", async (req, res) => {
+  const post = await Blog.findById(req.params.id);
+  if (!post) return res.send("Post not found");
+  res.render("post", { title: post.title, content: post.content });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
